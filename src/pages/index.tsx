5addbe0366487030f51adb738e2caba5c714db68
Akinola Femi <akinolafemi573@gmail.com>
2025-01-17 19:28:14 +0100

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,8 @@ const Index = () => {
         <nav className='nav'>
           <ul className='flex justify-center'>
             <li><Link href='/blog/blog'>Home</Link></li>
-            <li><Link href='/blog/blog'>Blog</Link></li>
-            <li><Link href='/blog/blog'>Equipment</Link></li>
+            <li><Link href='/'>Blog</Link></li>
+            <li><Link href='/'>Equipment</Link></li>
             <li><Link href='/blog/blog'>Booking</Link></li>
           </ul>
         </nav>
@@ -21,7 +21,7 @@ const Index = () => {
       <section className='mainSection'>
         <main className='mainSectionContent'>
           <h1 className='text-center text-4xl'>Welcome to GyM House</h1>
-          <p className='text-center text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.</p>
+          <p className='text-center text-lg'>Hi ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.</p>
         </main>
       </section>
     </div>
