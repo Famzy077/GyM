c4f99e9222e0162288bf6e08421e250681b4eb7f
Akinola Femi <akinolafemi573@gmail.com>
2025-01-18 21:23:50 +0100

    Add landing page features

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import '../globals.css'; // Import global styles here
 import Layout from '../layout'; // Import your layout if needed
-import type { AppProps } from 'next/app'; // Import AppProps type
+import type { AppProps } from 'next/app';
+import { Roboto } from 'next/font/google';
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className={roboto.className || 'fallback-sans-serif'}>
+          <Layout>
+        <Component {...pageProps} />
+        </Layout>
+    </div>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import img from '../../public/images/nav-logo.png'
 import Image from 'next/image'
+import 'next/font/google';
 import { useState, useEffect } from 'react';
 const Index: React.FC = () => {
 
@@ -26,38 +27,83 @@ const Index: React.FC = () => {
     }, [scroll]);
   return (
     <div>
-      <section className='mainSection'>
-        <main className='mainSectionContent'>
-          <header className='header flex justify-between bg- text-white h-[3rem] place-items-end fixed top-0 w-[100%] z-50'>
-            <div className="logo text-center border-2 border-blue-800 w-[100%]">
-              <Link className='' href="#"><Image src={img} width={100} height={100} alt='logoIcon'></Image></Link>
+      <header className='header flex justify-between py-8 text-white h-[3rem] px-4 place-items-center w-[100%] z-50 bg-gray-800'>
+            <div className="logo text-center w-[100%]">
+              <Link className='' href="#"><Image src={img} width={130} height={130} alt='logoIcon'></Image></Link>
               <div><i className="fa-solid fa-bars"></i></div>
             </div>
-            <nav className='nav'>
-              <ul className='flex justify-center gap-2'>
+            <nav className='nav w-[100%]'>
+              <ul className='flex justify-start gap-2 text-xl'>
                 <li><Link href='/blog/blog'>Home</Link></li>
                 <li><Link href='/'>Blog</Link></li>
-                <li><Link href='/'>Equipment</Link></li>
-                <li><Link href='/blog/blog'>Booking</Link></li>
+                <li><Link href='/'>Shop</Link></li>
+                <li><Link href='/blog/blog'>Contact</Link></li>
+                <li><Link href='/blog/blog'>FAQs</Link></li>
               </ul>
             </nav>
           </header>
+      <section className='mainSection'>
+        <main className='mainSectionContent'>
+          
 
-          <div className='pt-[60px]'>
-            <h1 className='text-center text-4xl'>Welcome to GyM House</h1>
-            <p className='text-center text-lg'>Hi ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.</p>
+          <div className='pt-[60px] h-[90vh] flex place-items-center'>
+            <main className='leading-[6rem] text-[7rem] font-extrabold'>
+              <h1 className='text-start mx-16  text-white w-[50%] font-bold'>Ultimate</h1>
+              <h1 className='text-start mx-16 font-semibold text-white w-[50%]'>Crossfit</h1>
+              <h1 className='text-start mainH1 mx-16 font-semibold text-white w-[50%]'> Facility</h1>
+            </main>
+          </div>
+          <div className="scrolling-text text-white justify-evenly w-[fit-content] text-center gap-3">
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#Gym</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#training</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#health</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#sport</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#workout</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#fitness</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#progress</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#pumping</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#coach</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#crossfit</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#trainer</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#pool</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#personal</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#swimming</h3>
+            <h3 className='bg-gray-900 text-yellow-700 rounded-lg p-2 text-xl'>#bodybuilding</h3>
           </div>
         </main>
       </section>
 
-      <section>
-        <div className="section1 h-[120vh]">
-
+      <section className='section2 text-white p-8 h-[100vh]'>
+        <div className='text-center'>
+          <h2 className='text-xl font-semibold'>What we offer</h2>
+          <h1 className='text-5xl font-bold px-32'>Achieve amazing results <br /> with our services</h1>
+          <div className='place-content-center flex'>
+            <Image className='text-center rounded-xl p-1 bg-white h-[40px] w-[40px] m-2' src='data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2225%22%20height%3D%2215%22%20viewBox%3D%220%200%2025%2015%22%3E%3Ctext%20transform%3D%22translate(0%2015)%20rotate(-90)%22%20font-size%3D%2229px%22%20font-family%3D%22PlayfairDisplay-Regular%2C%20Playfair%20Display%22%3E%3Ctspan%20x%3D%220%22%20y%3D%2220%22%3E%C2%AB%3C%2Ftspan%3E%3C%2Ftext%3E%3C%2Fsvg%3E' alt='image' width={70} height={70}></Image>
+          </div>
         </div>
-        <main className='h-[200vh]'></main>
+
+        <div className='sectionBox'>
+            <main className='py-10 px-4 mainBox'>
+              <h1 className='text-center text-xl font-semibold'>Best Equipment</h1>
+              <p className='text-gray-200 text-center text-[19px]'>Consectetur adipiscing elit, sed do eiusmod tempo.</p>
+            </main>
+            <main>
+              <h1>Training Plan</h1>
+              <p>Consectetur adipiscing elit, sed do eiusmod tempo.</p>
+            </main>
+            <main>
+              <h1>Nutrition plan</h1>
+              <p>Consectetur adipiscing elit, sed do eiusmod tempo.</p>
+            </main>
+            <main>
+              <h1>Swimming poor</h1>
+              <p>Consectetur adipiscing elit, sed do eiusmod tempo.</p>
+            </main>
+        </div>
       </section>
+
     </div>
+
   );
 }
-
 export default Index;
