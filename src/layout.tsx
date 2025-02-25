7d478890984b8dbd833316c446f519a804001a8f
Akinola Femi <akinolafemi573@gmail.com>
2025-02-25 07:16:41 +0100

    done with home page

--- src/layout.tsx
+++ src/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <main>
       <Head>
+        <title>Gym Website</title>
         <Link href='<link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Edu+AU+VIC+WA+NT+Arrows&family=Jacquarda+Bastarda+9&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lora:ital,wght@0,400..700;1,400..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik+Vinyl&display=swap" rel="stylesheet">'></Link>
